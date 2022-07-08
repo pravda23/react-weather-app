4f5b0dba50ed4cfb56e87af664e21a50f1679f84
pravda23 <pravda23@gmail.com>
2022-07-08 15:25:54 +0400

    api call and render working

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,30 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function Card() {
-  fetch(
-    "http://api.weatherapi.com/v1/current.json?key=47b6acea5d204134b4661938220707&q=Batumi&aqi=no"
-  ).then((response) => {
-    response.json().then((json) => console.log(json));
-  });
+  const [cardData, setCardData] = useState([]);
+
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const api = await fetch(
+          "http://api.weatherapi.com/v1/current.json?key=47b6acea5d204134b4661938220707&q=Cape_Town&aqi=no"
+        );
+        const data = await api.json();
+        console.log(data);
+        setCardData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getWeather();
+  }, []);
+
   return (
-    <>
-      <div className="Card border">
-        <div>Card header</div>
-        <div></div>
-      </div>
-    </>
+    <div>
+      <h1>{cardData.location.name}</h1>
+      <h1>{cardData.current.temp_c}</h1>
+    </div>
   );
 }
 
