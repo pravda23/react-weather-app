2e2e25ecf40eba15b322450a2ed61e9d1374a890
pravda23 <pravda23@gmail.com>
2022-07-28 11:04:19 +0200

    working live search, errors handled

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,7 +1,3 @@
-// TO DO: user inputs city name, which then updates Results component placeholder with api call weather data
-
-// TO DO: pre-search placeholder text within results area
-
 import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
@@ -9,60 +5,69 @@ import { useState, useEffect } from "react";
 const Results = (props) => {
   const [forecast, setForecast] = useState([]);
   const [location, setLocation] = useState([]);
-  const [cityProp, setCityProp] = useState([]);
-
-  console.log("props.cityProp: " + props.cityProp);
 
   useEffect(() => {
-    const getWeather = async () => {
-      try {
-        fetch(
-          `http://api.weatherapi.com/v1/forecast.json?key=47b6acea5d204134b4661938220707&q="${props.cityProp}"&days=3&aqi=no`
-        ).then((response) => {
-          response.json().then((data) => {
-            setForecast(data.forecast);
-            setLocation(data.location);
-            setCityProp(props.cityProp);
+    if (props.cityProp === "") {
+      console.log("empty");
+    } else {
+      const getWeather = async () => {
+        try {
+          fetch(
+            `http://api.weatherapi.com/v1/forecast.json?key=47b6acea5d204134b4661938220707&q="${props.cityProp}"&days=3&aqi=no`
+          ).then((response) => {
+            response.json().then((data) => {
+              setForecast(data.forecast);
+              setLocation(data.location);
+            });
           });
-        });
-      } catch (err) {
-        console.log("Error");
-      }
-    };
-    getWeather();
+        } catch (err) {
+          console.log("Error");
+        }
+      };
+      getWeather();
+    }
   }, [props.cityProp]);
 
-  const renderedContent = forecast.forecastday
-    ? forecast.forecastday.map((item) => {
-        return (
-          <div key={item.date_epoch} className="dateTempContainer">
-            <div className="dateContainer">
-              <li>
-                {new Date(item.date).toLocaleDateString("en-gb", {
-                  weekday: "short",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </li>
-            </div>
-
-            <div className="tempContainer">{item.day.maxtemp_c}</div>
-          </div>
-        );
-      })
-    : null;
+  // pre-search explainer placeholder text
+  if (!props.cityProp) {
+    return "Start typing a city name to search ";
+  }
 
   return (
     <div>
       <h2>
-        {location.name}, {location.country}
+        {location ? location.name : null}
+        {location ? "," : "City not found"}
+        {location ? location.country : null}
       </h2>
 
-      <div>{renderedContent}</div>
+      <div>
+        {forecast && forecast.forecastday
+          ? forecast.forecastday.map((item) => {
+              return (
+                <div key={item.date_epoch} className="dateTempContainer">
+                  <div className="dateContainer">
+                    <li>
+                      {new Date(item.date).toLocaleDateString("en-gb", {
+                        weekday: "short",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </li>
+                  </div>
+
+                  <div className="tempContainer">{item.day.maxtemp_c}</div>
+                </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
 
 export default Results;
 
-// // What is the ternary conditional surrounding renderedContent? This prevents forecastday from being undefined. The app runs an async function api call, but forecastday is called before it can return this data. The conditional logic basically says ignore the renderedContent code block unless there's a change to the state value, originally defined as an empty array in useState([]) and then updated with the api data.
+// // The ternary conditional surrounding renderedContent prevents forecastday from being undefined. The app runs an async api call, but forecastday is called before it can return this data. The conditional logic basically says ignore the renderedContent code block unless there's a change to the state value, originally defined as an empty array in useState([]) and then updated with the api data.
+
+// At one point, the data wasn't being passed to the api call because of the useEffect dependency array (the ",[]" after useEffect). The array tells the program what incoming data to monitor for. Being blank, it didn't understand that it was 'listening' for props.cityProps
