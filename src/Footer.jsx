9fd064be87620208fc491f431cd1d997b47026c7
pravda23 <pravda23@gmail.com>
2022-08-04 08:51:04 +0200

    include footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="siteFooterContainer">© 2022 Lightshade Design</div>
+    </>
+  );
+}
+
+export default Footer;
